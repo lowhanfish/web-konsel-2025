60f73a01de2c8c5e4cfa5c4ac4fef75a5460901a
lowhanfish <lowhanfish@gmail.com>
2025-06-05 15:42:45 +0800

    Design Profile Component - Page Home

--- web-konsel/src/App.tsx
+++ web-konsel/src/App.tsx
@@ -4,7 +4,8 @@ import { Routes, Route } from "react-router-dom";
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/css/App.css'
+import './assets/css/Fonts.css'
 
 
 
--- web-konsel/src/pages/home/homeProfile.tsx
+++ web-konsel/src/pages/home/homeProfile.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function homeProfile() {
   return (
-    <div className='bgx bg-profile'>
+    <div className='bgx2 bg-profile'>
       <div className='profileTop'>
         <div className='profileLeft'>
           <img className='profileImgLeft' src="/images/label2.png" alt="" />
@@ -12,10 +12,35 @@ function homeProfile() {
         </div>
         <div className='profileRight'>
           <img className='profileImgRight' src="/images/wakil.png" alt="" />
+          <div>
+            <img className='imgWabup_tag' src="/images/wabup_tag.png" alt="" />
+          </div>
         </div>
       </div>
+
       <div className='profileBottom'>
-        a
+        <div className='profileBottomContainer'>
+
+          <a href="javascript:void(0);" className='profileBottomContainOutside'>
+            <img className='imgProfileIcon3' src="/images/profilbupati.png" alt="" />
+          </a>
+          <a href="javascript:void(0);" className='profileBottomContainOutside'>
+            <img className='imgProfileIcon3' src="/images/profilwakil.png" alt="" />
+          </a>
+          <a href="javascript:void(0);" className='profileBottomContainOutside'>
+            <img className='imgProfileIcon3' src="/images/profilsekda.png" alt="" />
+          </a>
+          <a href="javascript:void(0);" className='profileBottomContainOutside'>
+            <img className='imgProfileIcon3' src="/images/profilvisi.png" alt="" />
+          </a>
+          <a href="javascript:void(0);" className='profileBottomContainOutside'>
+            <img className='imgProfileIcon3' src="/images/profilmisi.png" alt="" />
+          </a>
+          <a href="javascript:void(0);" className='profileBottomContainOutside'>
+            <img className='imgProfileIcon3' src="/images/profilsejarah.png" alt="" />
+          </a>
+
+        </div>
       </div>
     </div>
   )
