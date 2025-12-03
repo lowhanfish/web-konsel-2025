d7acfbec1e7fe723c20fa494253e5802f1c1fc09
lowhanfish <lowhanfish@gmail.com>
2025-12-03 10:49:50 +0800

    test

--- web-konsel/src/pages/home/homeRecentNews.tsx
+++ web-konsel/src/pages/home/homeRecentNews.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function homeRecentNews() {
   return (
-    <div className='bgx bg-news'>
-      <h1>Home News</h1>
+    <div className='bgx3 bg-news'>
+      <div className='h_recentNewsTitle'>RECENT NEWS</div>
+
+      <div>
+
+      </div>
     </div>
   )
 }
