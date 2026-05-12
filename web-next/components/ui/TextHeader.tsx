14b1ccb41a9a5673922ad3e93c5c123ff730856a
lowhanfish <lowhanfish@gmail.com>
2026-05-12 23:12:08 +0700

    modify style1

--- web-next/components/ui/TextHeader.tsx
+++ web-next/components/ui/TextHeader.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+// import React from 'react'
 
-const TextHeader = () => {
-    return (
-        <div>
-            <div className='flex flex-col justify-center items-center'>
-                <p className='text-[35px] md:text-[55px] font-bold'>Website Resmi</p>
-                <p className='text-[18px] -mt-5'>Kabupaten Konawe Selatan</p>
-            </div>
+// const TextHeader = () => {
+//     return (
+//         <div>
+//             <div className='flex flex-col justify-center items-center'>
+//                 <p className='text-[35px] md:text-[55px] font-bold'>Website Resmi</p>
+//                 <p className='text-[18px] -mt-5'>Kabupaten Konawe Selatan</p>
+//             </div>
 
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default TextHeader
+// export default TextHeader
